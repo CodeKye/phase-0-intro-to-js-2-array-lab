dfecc33e3113d701acbcd2b7cda9b76fbe05e9c2
David Kye <davidkye2.0@gmail.com>
2022-06-01 12:41:32 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,33 +1,46 @@
 // Write your solution here!
 const cats = ["Milo", "Otis", "Garfield"]
 
+beforeEach(function () {
+    cats.length = 0;
+
+    cats.push("Milo", "Otis", "Garfield");
+})
+
 function destructivelyAppendCat() {
     cats.push("Ralph");
 }
+
 function destructivelyPrependCat() {
     cats.unshift("Bob");
 }
+
 function destructivelyRemoveLastCat() {
-    cats.pop()
+    cats.pop();
 }
+
 function destructivelyRemoveFirstCat() {
-    cats.shift()
+    cats.shift();
 }
+
 function appendCat() {
     var newCat = cats.slice()
     newCat.push("Broom");
     return newCat
 }
+
 function prependCat() {
-    var strongCat = cats.slice()
-    strongCat.unshift("Arnold");
-    return strongCat
+    var addCat = cats.slice()
+    addCat.unshift("Arnold");
+    return addCat
 }
+
 function removeLastCat() {
     var deadCat = cats.slice()
     deadCat.pop()
     return deadCat
 }
+
 function removeFirstCat() {
     var runCat = cats.slice()
     runCat.shift()
